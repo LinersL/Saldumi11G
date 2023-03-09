5124dc6a994ce75b6d66361c678c84371399c966
Liners Laizāns <ll2765@gnvsk.lv>
2023-03-09 23:35:35 +0200

    Pabeigts darbs

--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,7 @@ namespace LL_Saldumi11
 {
     public partial class Form1 : Form
     {
-        double cena = 0.00;
+        double cena = 0.00;  //define "cena"
 
         public Form1()
         {
@@ -33,59 +33,55 @@ namespace LL_Saldumi11
         private void button1_Click(object sender, EventArgs e)
         {
 
-            CENAS();
-            Aprekins();
+            CENAS(); //ar pogas spiedienu izsauc cenu funkciju
+            Aprekins(); //ar pogas spiedienu izsauc aprēķinu funkciju
         }
-        private void CENAS()
+        private void CENAS() //izveidota Cenas funkcija
         {
             if (checkBox1.Checked == true && checkBox2.Checked == true && checkBox3.Checked == true)
             {
-                cena = 20.3;
+                cena = 20.3; //nosaka cenu ja atķeksētas visas kastītes
             }
             else if (checkBox1.Checked == true && checkBox2.Checked == true && checkBox3.Checked == false)
             {
-                cena = 16.1;
+                cena = 16.1; //nosaka cenu ja atķeksētas pirmās divas kastītes
 
             }
             if (checkBox1.Checked == true && checkBox2.Checked == false && checkBox3.Checked == true)
             {
-                cena = 14.08;
-               
+                cena = 14.08;  //nosaka cenu ja atķeksētas pirmā un pēdējā kastīte
+
             }
             else if (checkBox1.Checked == false && checkBox2.Checked == true && checkBox3.Checked == true)
             {
-                cena = 10.42;
-              
+                cena = 10.42;  //nosaka cenu ja atķeksētas pēdējās divas kastītes
+
             }
             if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == true)
             {
-                cena = 4.20;
+                cena = 4.20;  //nosaka cenu ja atķeksētas visas kastītes
             }
             else if (checkBox1.Checked == true && checkBox2.Checked == false && checkBox3.Checked == false)
             {
-                cena = 9.88;
+                cena = 9.88;  //nosaka cenu ja atķeksēta pirmā kastīte
             }
             if (checkBox1.Checked == false && checkBox2.Checked == true && checkBox3.Checked == false)
             {
-                cena = 6.22;
+                cena = 6.22; //nosaka cenu ja atķeksēta otrā kastīte
             }
             if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false)
             {
-                MessageBox.Show("Nav neviens saldumu veids atlasits");
+                MessageBox.Show("Nav neviens saldumu veids atlasits"); // izvada ka nav neviena kastīte atķeksēta
             }
 
         }
-        private void Aprekins()
+        private void Aprekins() //izveido aprēķinu funkciju
         {
 
-            double sk1 = Convert.ToDouble(Summa.Text);
-            double dati = sk1 / cena;
-            if (dati <= 0)
-            {
-                MessageBox.Show("!!TEV NAV NAUDAS!!");
-            }
-            double rezultats = Math.Round(dati, 3);
-            Dati_TB.Text = rezultats.ToString();
+            double sk1 = Convert.ToDouble(Summa.Text); // Textbox tekstu pārvērš par skaitli
+            double dati = sk1 / cena; //veic aprēķinus
+            double rezultats = Math.Round(dati, 3); //noapaļo rezultātu līdz trīs skaitļiem aiz komata
+            Dati_TB.Text = rezultats.ToString();  //pārvērš rezultātu uz string
 
         }
 
@@ -122,38 +118,38 @@ namespace LL_Saldumi11
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string ceksDirectory = Application.StartupPath + "\\Ceks\\";
+            string ceksDirectory = Application.StartupPath + "\\Ceks\\";  // sameklē un atver failu ar nosaukumu 'Ceks'
             Directory.CreateDirectory(ceksDirectory);
 
-            string fileName = vardsBox1.Text +" "+ dateTimePicker1.Text + " .txt";
-            string filePath = Path.Combine(ceksDirectory, fileName);
+            string fileName = vardsBox1.Text +" "+ dateTimePicker1.Text + " ceks.txt"; // izveido teksta failu ar specifisku nosaukumu
+            string filePath = Path.Combine(ceksDirectory, fileName); 
 
-            using (StreamWriter writer = new StreamWriter(filePath))
+            using (StreamWriter writer = new StreamWriter(filePath))  // rāda kas tiek izmantot jeb 'streamwriter' 
             {
-                writer.WriteLine(Vards.Text + ": " + vardsBox1.Text);
-                writer.WriteLine(Datums.Text + ": " + dateTimePicker1.Text);
-                writer.WriteLine(Nsumma.Text + ": " + Summa.Text + "€");
-                writer.WriteLine(daudz.Text + ": " + Dati_TB.Text + "kg");
+                writer.WriteLine(Vards.Text + ": " + vardsBox1.Text); // ievada teksta failā lietotāja vārdu
+                writer.WriteLine(Datums.Text + ": " + dateTimePicker1.Text); // ievada teksta failā lietotāja ievadīto datumu
+                writer.WriteLine(Nsumma.Text + ": " + Summa.Text + "€"); // ievada teksta failā lietotāja pieejamās naudas summu
+                writer.WriteLine(daudz.Text + ": " + Dati_TB.Text + "kg"); // ievada teksta failā lietotāja aprēķināto saldumu daudzumu
 
-                string selected = "";
+                string selected = ""; // zveido jaunu tukšu virkni "selected", kuru izmantosim, lai apkopotu visas atlasītās izvēles.
 
-                if (checkBox1.Checked)
-                    selected += checkBox1.Text + ", ";
+                if (checkBox1.Checked) // Pārbauda, vai "checkBox1" ir atzīmēts, kas nozīmē, ka tas ir atlases statusā.
+                    selected += checkBox1.Text + ", "; // Ja "checkBox1" ir atzīmēts, tad pievieno tā tekstu "selected" virknē, un iekļauj arī komatu un atstarpi pēc tā, lai varētu atšķirt no citām izvēlēm.
 
-                if (checkBox2.Checked)
-                    selected += checkBox2.Text + ", ";
+                if (checkBox2.Checked) // Pārbauda, vai "checkBox2" ir atzīmēts.
+                    selected += checkBox2.Text + ", "; // Ja "checkBox2" ir atzīmēts, tad pievieno tā tekstu "selected" virknē, un iekļauj arī komatu un atstarpi pēc tā.
 
-                if (checkBox3.Checked)
-                    selected += checkBox3.Text;
+                if (checkBox3.Checked) // Pārbauda, vai "checkBox3" ir atzīmēts.
+                    selected += checkBox3.Text;  // Ja "checkBox3" ir atzīmēts, tad pievieno tā tekstu "selected" virknē, bet šoreiz bez komata un atstarpes.
 
-                if (selected != "")
+                if (selected != "") // Pārbauda, vai "selected" virkne nav tukša, kas nozīmē, ka ir kāds saldumu veids atlases statusā.
                 {
-                    selected = selected.TrimEnd(',', ' ');
-                    writer.WriteLine("Salduma izvele: " + selected);
+                    selected = selected.TrimEnd(',', ' '); // Ja "selected" virknē ir iekļauti saldumi, tad noņem beigās iekļauto komatu un atstarpes, lai varētu izvadīt pareizu tekstu.
+                    writer.WriteLine("Salduma izvele: " + selected); // Izvada tekstu, kas ietver tekstu "Salduma izvēle" un visus atlasītos saldumus.
                 }
-                else
+                else // Ja neviens saldumu veids nav atlasīts, tad izpildās šī daļa.
                 {
-                    writer.WriteLine("Nav neviens saldumu veids atlasits");
+                    writer.WriteLine("Nav neviens saldumu veids atlasits"); // Izvada tekstu, kas paziņo, ka nav atlasīts neviens saldumu veids.
                 }
 
             }
